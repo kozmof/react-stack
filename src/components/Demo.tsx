5788d49e23812edfa02da2e60eed3597f0c5f0b1
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-05 19:21:09 +0900

    Change tab spaces

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -2,23 +2,23 @@ import * as React from "react";
 import { StackProps, ImmutableStack } from "./Stack";
 
 class Item {
-   constructor(public id: number, public name: string) {}
+  constructor(public id: number, public name: string) {}
 }
 
-export let props: StackProps<Item> = {
-    items: [new Item(0, "Red"), new Item(1, "Kasumi"), new Item(1, "Green")],
+export const props: StackProps<Item> = {
+  items: [new Item(0, "Red"), new Item(1, "Kasumi"), new Item(2, "Green")],
 
-    mapCallback: (item) => {
-        return(
-            <tr key={ item.id }>     
-                <td>{ item.name }</td>
-            </tr>
-        );
-    }
+  mapCallback: (item) => {
+    return(
+      <tr key={item.id}>     
+        <td>{item.name}</td>
+      </tr>
+    );
+  }
 }
 
 export class StackDemo extends ImmutableStack<StackProps<Item>, {}> {
-    constructor(props: StackProps<Item>){
-        super(props, {})
-    }
+  constructor(props: StackProps<Item>){
+    super(props, {})
+  }
 }
