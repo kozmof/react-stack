85fe1124c8a3dbfbce416bc653f0ac704d230b75
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-06 11:22:24 +0900

    Add Mutable Stack

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 
 export interface StackProps<T> {
-  items : Array<T>;
   mapCallback(item: T): JSX.Element;
 }
 
-export class ImmutableStack<P extends StackProps<any>, S> extends React.Component<P, S> {
+export interface ImmutableStackProps<T> extends StackProps<T> {
+  items : Array<T>;
+}
+
+export interface MutableStackState<T> {
+  items : Array<T>;
+}
+
+export class ImmutableStack<P extends ImmutableStackProps<any>, S> extends React.Component<P, S> {
   constructor(props: P, initial_state: S){
     super(props); 
     this.state = initial_state;
@@ -14,6 +21,26 @@ export class ImmutableStack<P extends StackProps<any>, S> extends React.Componen
 
   render(){
     const items = this.props.items || [];
+    const rows = items.map(this.props.mapCallback);
+    return(
+      <table> 
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export class MutableStack<P extends StackProps<any>, S extends MutableStackState<any>> extends React.Component<P, S> {
+  constructor(props: P, initial_state: S){
+    super(props); 
+    this.state = initial_state;
+    this.props.mapCallback.bind(this);
+  }
+
+  render(){
+    const items = this.state.items || [];
     const rows = items.map(this.props.mapCallback);
     return(
       <table> 
